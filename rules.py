e7c29c9d8f16b7b241175fbdead676ed39c7e7ed
gilbertliou <gilbert.liou.tw@gmail.com>
2015-11-17 10:41:08 +0800

    add actios & fix bug in rules.py

--- rules.py
+++ rules.py
@@ -3,6 +3,7 @@
 
 import os
 import itertools as it
+from pdb import set_trace
 
 
 SIZE = [6, 7]
@@ -11,6 +12,7 @@ STATE = ' ' * N
 
 
 def show(state):
+    # Show board state
     print 'Board State'
     for r in xrange(SIZE[0]):
         i0 = (r) * SIZE[1]
@@ -18,38 +20,43 @@ def show(state):
         print '||' + '|'.join(state[i0:i1]) + '||'
 
 
-def pos(r, c):
-    assert(c < SIZE[1])
-    assert(r < SIZE[0])
-    return r * (1 + SIZE[0]) + c
+def posidx(r, c):
+    # Position index
+    assert c < SIZE[1], 'wrong column %s' % c
+    assert r < SIZE[0], 'wrong row %s' % r
+    return r * SIZE[1] + c
 
 
 def action(state, c, sgn):
+    # Put sgn at c column then return new state
     i0 = c
     if state[c] != ' ':
-        raise Exception('No possible position!!')
+        raise Exception('No possible position in %d!!' % c)
 
     for r in xrange(1, SIZE[0]):
-        i = pos(r, c)
+        i = posidx(r, c)
         if (state[i] != ' '):
             break
         i0 = i
     return state[:i0] + sgn + state[(i0 + 1):]
 
 
+def actions(state):
+    # Return Possible actions
+    for c in xrange(SIZE[1]):
+        if state[c] == ' ':
+            yield c
+
+
 def newstate(state, r, c, sgn):
-    n = pos(r, c)
-    print n
-    assert(state[n] == ' ')
-    return state[:n] + sgn + state[(n + 1):]
-
-
-def state1(state, n, sgn):
+    # Put sgn at (r, c) then return new state
+    n = posidx(r, c)
     assert(state[n] == ' ')
     return state[:n] + sgn + state[(n + 1):]
 
 
 def chkDual(state):
+    # Check dual step
     cnt1 = sum([(x == 'O') for x in state])
     cnt2 = sum([(x == 'X') for x in state])
     assert (cnt1 - cnt2) <= 1, 'Dual step'
@@ -61,35 +68,62 @@ def poswin():
     # line for --
     for r in xrange(SIZE[0]):
         for c in xrange(SIZE[1] - 3):
-            yield [pos(r, c + i) for i in xrange(4)]
+            yield [posidx(r, c + i) for i in xrange(4)]
     # line fo |
     for c in xrange(SIZE[1]):
         for r in xrange(SIZE[0] - 3):
-            yield [pos(r + i, c) for i in xrange(4)]
+            yield [posidx(r + i, c) for i in xrange(4)]
     # line for \
     for r in xrange(SIZE[0] - 3):
         for c in xrange(SIZE[1] - 3):
-            yield [pos(r + i, c + i) for i in xrange(4)]
+            yield [posidx(r + i, c + i) for i in xrange(4)]
     # line for /
     for r in xrange(SIZE[0] - 3):
         for c in xrange(3, SIZE[1]):
-            yield [pos(r + i, c - i) for i in xrange(4)]
+            yield [posidx(r + i, c - i) for i in xrange(4)]
 
 
 def listwin():
+    # list cases of who's win
     for idxs in poswin():
         s = STATE
+        print idxs
         for idx in idxs:
-            s = state1(s, idx, 'O')
+            c = idx % SIZE[1]
+            r = (idx - c) / SIZE[0]
+            s = newstate(s, r, c, 'O')
         show(s)
 
 
 def chkwin(state):
+    # Check who is winner according to possible rules
     for idxs in poswin():
-        for sgn in ('O', 'X'):
-            if all([(state[i] == sgn) for i in idxs]):
-                return sgn
+        cnt = sum(map(reward, idxs))
+        if cnt == 3:
+            return 'O'
+        elif cnt == -3:
+            return 'X'
     return None
+
+
+def reward(sgn):
+    # Return reward by sgn
+    if sgn == 'O':
+        return 1
+    elif sgn == 'X':
+        return -1
+    else:
+        return 0
+
+
+def chkwho(state):
+    # Get who's turn
+    cnt = sum(map(reward, state))
+    if cnt == 1:
+        return 'X'
+    elif cnt == 0:
+        return 'O'
+    raise Exception('Wrong turn!!')
 
 
 if __name__ == '__main__':
@@ -103,10 +137,4 @@ if __name__ == '__main__':
     #     chkDual(s1)
 
     listwin()
-
-    idxs = it.islice(poswin(), 50, 51).next()
-    for idx in idxs:
-        STATE = state1(STATE, idx, 'X')
-    show(STATE)
-    print chkwin(STATE)
 
